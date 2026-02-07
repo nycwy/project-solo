404b94c4d66aa3a51c86e359e7b634d4267aefbc
Nycwy <stuxy.1129@gmail.com>
2026-02-07 15:35:05 +0545

    Handle Register

--- src/features/auth/Register.jsx
+++ src/features/auth/Register.jsx
@@ -3,15 +3,28 @@ import Input from '../../components/Input'
 import Heading from '../../components/Heading';
 import Button from '../../components/Button';
 import Already from '../../components/Already';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth, db } from '../../services/firebase';
+import { doc, serverTimestamp, setDoc } from 'firebase/firestore';
 
 const Register = () => {
     const [email, setEmail] = useState('');
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
-    const handleRegister = () => {
-
+        const handleRegister = async () => {
+        try {
+            if (!email || !username || !password) {
+                return;
+            }
+            const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+            const userRef = doc(db, "users", userCredential.user.uid);
+            await setDoc(userRef, { email, username, created_at: serverTimestamp() });
+        } catch (error) {
+            console.log("Error: ", error);
+        }
     }
+
     return (
         <div className='flex items-center justify-center min-h-screen bg-gray-100'>
             <div className='w-full max-w-md p-8 space-y-6 bg-white rounded-xl shadow-lg border border-gray-100'>
