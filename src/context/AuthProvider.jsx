cb579c509a57fc55d3f487d37a31f04bff9b0745
Nycwy <stuxy.1129@gmail.com>
2026-02-07 15:57:45 +0545

    Auth Context add

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,52 +1,30 @@
-import { createContext, useEffect, useState } from "react";
-import {
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    signOut,
-    onAuthStateChanged
-} from "firebase/auth";
-import { auth } from "../services/firebase";
+import { onAuthStateChanged } from 'firebase/auth';
+import React, { useEffect, useState } from 'react'
+import { auth, db } from '../services/firebase';
+import { doc, getDoc } from 'firebase/firestore';
+import { AuthContext } from './AuthContext'
 
-const AuthContext = createContext();
-
-export function AuthProvider({ children }) {
-    const [currentUser, setCurrentUser] = useState(null);
-    const [loading, setLoading] = useState(true);
-
-    // 1. Sign Up Function
-    function signup(email, password) {
-        return createUserWithEmailAndPassword(auth, email, password);
-    }
-
-    // 2. Login Function
-    function login(email, password) {
-        return signInWithEmailAndPassword(auth, email, password);
-    }
-
-    // 3. Logout Function
-    function logout() {
-        return signOut(auth);
-    }
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
 
     useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, (user) => {
-            setCurrentUser(user);
-            setLoading(false);
+        const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
+            if (currentUser) {
+                const userRef = doc(db, "users", currentUser.uid);
+                const fetchedUser = await getDoc(userRef);
+                setUser({ ...fetchedUser.data(), id: fetchedUser.id });
+            } else {
+                setUser(null);
+            }
         });
-
-        return unsubscribe; // Cleanup listener
-    }, []);
-
-    const value = {
-        currentUser,
-        signup,
-        login,
-        logout
-    };
+        return unsubscribe;
+    })
 
     return (
-        <AuthContext.Provider value={value}>
-            {!loading && children}
+        <AuthContext.Provider value={{ user }}>
+            {children}
         </AuthContext.Provider>
-    );
+    )
 }
+
+export default AuthProvider
